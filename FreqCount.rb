433c60c07711ae34af3c5ce12c53a2101ad332bc
Rushikesh <rushiselldo>
2025-01-20 11:01:50 +0530

    Learning upto basic logic codes

---
+++ FreqCount.rb
@@ -0,0 +1,43 @@
+# puts "Type something here..."
+# text = gets.chomp
+
+# # Split the text into words
+# words = text.split
+
+# # Initialize the frequencies hash with a default value of 0
+# frequencies = Hash.new(0)
+
+# # Iterate over the words array
+# words.each do |word|
+#   frequencies[word] += 1 # Increment the frequency of each word
+# end
+
+# # Output the frequencies
+# puts frequencies
+
+
+#Sort by frq
+
+puts "Type something here..."
+text = gets.chomp
+
+# Split the text into words
+words = text.split
+
+# Initialize the frequencies hash with a default value of 0
+frequencies = Hash.new(0)
+
+# Iterate over the words array
+words.each do |word|
+  frequencies[word] += 1 # Increment the frequency of each word
+end
+
+# Output the frequencies
+
+frequencies=frequencies.sort_by do |name,count|
+  count
+end
+frequencies.reverse!
+frequencies.each { |name,count|
+  puts "#{name} #{count}"
+}
